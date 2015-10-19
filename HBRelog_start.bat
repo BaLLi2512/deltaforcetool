a6d3cf3a084cd041b38c934759289191e7a69793
Maik Ballichar <maik@ballichar.de>
2015-10-19 18:04:27 +0200

    Fast StartUp Links
    Schnellstartdateien hinzugefügt

---
+++ HBRelog_start.bat
@@ -0,0 +1,18 @@
+@echo off
+tasklist /FI "IMAGENAME eq vgsjn.exe" 2>NUL | find /I /N "vgsjn.exe">NUL
+if "%ERRORLEVEL%"=="0" (
+goto withoutvgsjn
+) else (
+goto normal )
+
+:normal
+cd vgsjn
+start vgsjn.exe
+cd ..
+cd HBRelog\bin\Release
+start HBRelog.exe
+
+:withoutvgsjn
+cd HBRelog\bin\Release
+start HBRelog.exe
+exit
---
+++ HB_start.bat
@@ -0,0 +1,18 @@
+@echo off
+tasklist /FI "IMAGENAME eq vgsjn.exe" 2>NUL | find /I /N "vgsjn.exe">NUL
+if "%ERRORLEVEL%"=="0" (
+goto withoutvgsjn
+) else (
+goto normal )
+
+:normal
+cd vgsjn
+start vgsjn.exe
+cd ..
+cd Honorbuddy
+start Honorbuddy.exe
+
+:withoutvgsjn
+cd Honorbuddy
+start Honorbuddy.exe
+exit
